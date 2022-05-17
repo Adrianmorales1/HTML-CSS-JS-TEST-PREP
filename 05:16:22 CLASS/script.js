403039faa2f6a4b172a4521240d109b95fc45b64
Adrian Daniel Morales <104307198+Adrianmorales1@users.noreply.github.com>
2022-05-16 21:41:46 -0500

    Add files via upload

---
+++ 05:16:22 CLASS/script.js
@@ -0,0 +1,13 @@
+console.log("connected to JS");
+
+function changeTextInput(element) {
+    element.style.color = "lime";
+}
+
+function changeRColor(element) {
+    element.style.color = "red";
+}
+
+function changeBColor(element) {
+    element.style.color = "blue";
+}
